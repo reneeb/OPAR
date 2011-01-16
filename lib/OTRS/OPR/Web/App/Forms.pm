a910030ba27327c8737fd0ceeac8053a561006d6
Renee Baecker <github@renee-baecker.de>
2011-01-16 11:32:56 -0800

    stub for a module that provides some functionality for web forms in OPAR

---
+++ lib/OTRS/OPR/Web/App/Forms.pm
@@ -0,0 +1,20 @@
+package OTRS::OPR::Web::App::Forms;
+
+use strict;
+use warnings;
+
+use parent '';
+
+sub check_formid {
+    my ($self) = @_;
+    
+    return 1;
+}
+
+sub get_formid {
+    my ($self) = @_;
+    
+    return 1;
+}
+
+1;
