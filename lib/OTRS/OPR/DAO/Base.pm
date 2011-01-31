ce5e594f3e8aefb38ccf7bdb8743244eeda915ee
reneeb <github@renee-baecker.de>
2011-01-31 20:58:04 +0100

    improve DAO code base

--- lib/OTRS/OPR/DAO/Base.pm
+++ lib/OTRS/OPR/DAO/Base.pm
@@ -8,16 +8,50 @@ has _schema => (
 has _config => (
     is => 'rw',
 );
+has _flags => (
+    is      => 'rw',
+    isa     => 'HashRef',
+    default => sub { {} },
+    traits  => ['Hash'],
+    handles => {
+        delete_flag   => 'delete',
+        set_flag      => 'set',
+        count_flags   => 'count',
+        changed_attrs => 'kv',
+    },
+);
 
 sub _after_init {
     my ($self) = @_;
     
     # get all attributes of object and save initial value
     # this is used to find out, if the object should be saved on destruction
+    my $meta       = $self->meta;
+    my @attributes = $meta->get_attribute_list;
+    my @trackable  = map{ $meta->get_attribute( $_ ) }@attributes;
     
+    my %original_values;
+    for my $attribute ( @trackable ) {
+        if (
+            $attribute->isa( 'OTRS::OPR::App::AttributeInformation' ) &&
+            $attribute->is_trackable
+        ) {
+            my $name = $attribute->name;
+            $self->delete_flag( $name );
+        }
+    }
+}
+
+sub _dirty_flag {
+    my ($self,$name) = @_;
+    
+    $self->set_flag( $name => 1 );
 }
 
 sub _has_changed {
+    my ($self) = @_;
+    
+    $self->count_flags;
 }
 
 sub _save {
--- lib/OTRS/OPR/DAO/User.pm
+++ lib/OTRS/OPR/DAO/User.pm
@@ -1,16 +1,43 @@
 package OTRS::OPR::DAO::User;
 
 use Moose;
+use OTRS::OPR::App::AttributeInformation;
 
 extends 'OTRS::OPR::DAO::Base';
 
-has session_id    => ( is => 'rw' );
-has user_name     => ( is => 'rw' );
-has user_id       => ( is => 'ro' );
-has website       => ( is => 'rw' );
-has user_password => ( is => 'rw' );
-has mail          => ( is => 'rw' );
-has groups        => ( is => 'rw', isa => 'ArrayRef[Str]', auto_deref => 1 );
+for my $attribute ( qw(session_id user_name user_id user_password website mail active) ) {
+    has $attribute => (
+        metaclass    => 'OTRS::OPR::App::AttributeInformation',
+        is_trackable => 1,
+        is           => 'rw',
+        trigger      => sub{ shift->_dirty_flag( $attribute ) },
+    );
+}
+
+has user_object => (
+    is  => 'rw',
+    isa => 'Object',
+);
+
+has groups        => (
+    traits       => ['Hash'],
+    metaclass    => 'OTRS::OPR::App::AttributeInformation',
+    is_trackable => 1,
+    is           => 'rw',
+    isa          => 'HashRef[Str]',
+    auto_deref   => 1,
+    default      => sub { {} },
+    handles      => {
+        add_group    => 'set',
+        remove_group => 'delete',
+        has_group    => 'get',
+        group_list   => 'kv',
+    },
+    trigger      => sub{ shift->_dirty_flag( 'groups' ) },
+);
+
+after 'add_group'    => sub{ shift->_dirty_flag( 'groups' ) };
+after 'remove_group' => sub{ shift->_dirty_flag( 'groups' ) };
 
 sub BUILD {
     my ($self) = @_;
@@ -18,7 +45,57 @@ sub BUILD {
     $self->user_name( $user->user_name );
     $self->website( $user->website );
     $self->mail( $user->mail );
-    #$self->after_init();
+    
+    my @group_user_objects = $user->opr_group_user;
+    my @group_objects;
+    
+    for my $group_user_object ( @group_user_objects ) {
+        push @group_objects, $group_user_object->opr_group;
+    }
+    
+    for my $group_obj ( @group_objects ) {
+        $self->add_group( lc $group_obj->group_name, 1 );
+    }
+    
+    $self->user_object( $user );
+    
+    $self->_after_init();
+}
+
+sub DEMOLISH {
+    my ($self) = @_;
+    
+    return if !$self->_has_changed;
+        
+    my @changed_attrs = $self->changed_attrs;
+    my $user          = $self->user_object;
+    for my $attr_element ( @changed_attrs ) {
+        my $attr = $attr_element->[0];
+        if ( $attr ne 'groups' ) {
+            $user->$attr( $self->$attr() );
+        }
+        elsif ( $attr eq 'groups' ) {
+            $self->ask_table( 'opr_group_user' )->search({
+                user_id => $self->user_id
+            })->delete;
+            
+            GROUP:
+            for my $group ( $self->group_list ) {
+                my ($group_object) = $self->ask_table( 'opr_group' )->search({
+                    group_name => $group->[0],
+                });
+                
+                next GROUP if !$group_object;
+                
+                my ($group_user) = $self->ask_table( 'opr_group_user' )->create(
+                    group_id => $group_object->group_id,
+                    user_id  => $self->user_id,
+                );
+                $group_user->update;
+            }
+        }
+    }
+    $user->update;
 }
 
 no Moose;
