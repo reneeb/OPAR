908a803dac3d660b1a8e0eccdbb0996bcba61c8c
Renee Baecker <github@renee-baecker.de>
2011-02-15 09:35:14 -0800

    improve package search

--- lib/OTRS/OPR/DB/Helper/Package.pm
+++ lib/OTRS/OPR/DB/Helper/Package.pm
@@ -22,6 +22,7 @@ sub page {
         my $term = $params->{search} || '';
         $term    =~ tr/*/%/;
         for my $field ( 'opr_package_names.package_name', 'description' ) {
+            next unless $term;
             $search_clauses{$field} = { LIKE => '%' . $term . '%' };
         }
     }
@@ -30,35 +31,39 @@ sub page {
         $search_clauses{uploaded_by} = $params->{uploader};
     }
     
+    if ( !$params->{all} ) {
+        $search_clauses{is_in_index} = 1;
+    }
+    
     my $resultset = $self->table( 'opr_package' )->search(
         {
-            is_in_index => 1,
             %search_clauses,
         },
         {
-            page     => $page,
-            rows     => $rows,
-            order_by => 'package_id',
-            group_by => [ 'opr_package_names.package_name' ],
-            join     => 'opr_package_names',
+            page      => $page,
+            rows      => $rows,
+            order_by  => 'package_id',
+            group_by  => [ 'opr_package_names.package_name' ],
+            join      => 'opr_package_names',
+            '+select' => [ 'opr_package_names.package_name' ],
         },
     );
     
     my @packages = $resultset->all;    
     my $pages    = $resultset->pager->last_page || 1;
-    
+        
     my @packages_for_template;
     for my $package ( @packages ) {
         
         # show just a short excerpt of the text if it is too long
-        my $text = $package->package_name;
-        $text    = substr( $text, 0, 37 ) . '...' if $params->{short} and 40 < length $text;
+        my ($text) = $package->opr_package_names->package_name;
+        $text      = substr( $text, 0, 37 ) . '...' if $params->{short} and 40 < length $text;
         
         # show just a short excerpt of the description if it is too long
         my $desc = $package->description;
         $desc    = substr( $desc, 0, 57 ) . '...' if $params->{short} and 60 < length $desc;
         
-        my ($author) = $package->opr_package_author;
+        my ($author) = $package->opr_user;
         
         # create the infos for the template
         push @packages_for_template, {
@@ -66,6 +71,8 @@ sub page {
             VERSION     => $package->version,
             DESCRIPTION => $desc,
             AUTHOR      => ($author ? $author->user_name : '' ),
+            __SCRIPT__  => $self->base_url,
+            MYROLE      => '',  # TODO
             DATE        => time_to_date( $self, $package->upload_time ),
         };
     }
