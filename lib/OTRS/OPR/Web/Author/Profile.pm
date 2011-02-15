a9c5e49de7d580e52690679884acb9958a5d64f6
Renee Baecker <github@renee-baecker.de>
2011-02-15 09:29:02 -0800

    View component for author profile

---
+++ lib/OTRS/OPR/Web/Author/Profile.pm
@@ -0,0 +1,108 @@
+package OTRS::OPR::Web::Author::Profile;
+
+use strict;
+use warnings;
+
+use parent qw(OTRS::OPR::Web::App);
+
+use OTRS::OPR::DAO::User;
+use OTRS::OPR::Web::App::Forms     qw(check_formid get_formid);
+use OTRS::OPR::Web::App::Prerun    qw(cgiapp_prerun);
+
+sub setup {
+    my ($self) = @_;
+
+    $self->main_tmpl( $self->config->get('templates.author') );
+    
+    my $startmode = 'show';
+    my $param     = $self->param( 'run' );
+    if( $param ){
+        $startmode = $param;
+    }
+
+    $self->start_mode( $startmode );
+    $self->mode_param( 'run' );
+    $self->run_modes(
+        AUTOLOAD => \&show,
+        show     => \&show,
+        edit     => \&edit,
+        save     => \&save,
+    );
+}
+
+sub show : Permission( 'author' ) {
+    my ($self) = @_;
+    
+    my %info = $self->_user_to_hash;
+    
+    $self->template( 'author_profile' );
+    $self->stash(
+        %info,
+    );
+}
+
+sub edit : Permission( 'author' ) {
+    my ($self) = @_;
+    
+    my $formid = $self->get_formid;
+    
+    my %info = $self->_user_to_hash;
+    
+    $self->template( 'author_profile_edit' );
+    $self->stash(
+        %info,
+        FORMID => $formid,
+    );
+}
+
+sub save : Permission( 'author' ) {
+    my ($self) = @_;
+    
+    # get user input
+    my %params = $self->query->Vars;
+    
+    # TODO: form validation; on error -> show edit mask
+    
+    # save values - only a few fields can be changed
+    my $dao = OTRS::OPR::DAO::User->new(
+        user_id => $self->user->user_id,
+        _schema => $self->schema,
+    );
+    
+    my $password = '';
+    
+    $dao->user_password( $password ) if $password;
+    $dao->website( $params{website} );
+    $dao->mail( $params{mail} );
+    
+    my %info = $self->_user_to_hash( $dao );
+    
+    $self->template( 'author_profile_edit' );
+    $self->stash(
+        %info,
+    );
+    
+    # show success notification
+    $self->notify({
+        type             => 'success',
+        include          => 'notifications/generic_success',
+        SUCCESS_HEADLINE => 'Your changes have been saved',
+        SUCCESS_MESSAGE  => 'Your profile changes have been saved',
+    });
+}
+
+sub _user_to_hash {
+    my ( $self, $dao ) = @_;
+    
+    $dao ||= $self->user;
+    
+    my %info;
+    
+    for my $attr ( qw(user_name website mail) ) {
+        $info{$attr} = $dao->$attr();
+    }
+    
+    return %info;
+}
+
+1;
