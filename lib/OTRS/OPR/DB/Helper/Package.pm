2dc79637ce262666c3646eb53457f9aed3739200
Renee Baecker <github@renee-baecker.de>
2011-02-16 05:47:56 -0800

    create subroutine to transform object to hash, add new methods to get list of all versions and to check if a package already exists

--- lib/OTRS/OPR/DB/Helper/Package.pm
+++ lib/OTRS/OPR/DB/Helper/Package.pm
@@ -10,7 +10,32 @@ use OTRS::OPR::Web::Utils qw(time_to_date);
 our @EXPORT_OK = qw(
     page
     user_is_maintainer
+    package_exists
+    version_list
 );
+
+sub package_exists {
+    my ( $self, $name, $params ) = @_;
+    
+    return if !$name;
+    
+    my %where = (
+        package_name => $name,
+    );
+    
+    if ( $params->{version} ) {
+        $where{'opr_package.version'} = $params->{version};
+    }
+    
+    my ($package) = $self->table( 'opr_package_names' )->search(
+        \%where,
+        {
+            join => 'opr_package',
+        },
+    );
+    
+    return $package;
+}
 
 sub page {
     my ($self,$page,$params) = @_;
@@ -55,29 +80,58 @@ sub page {
     my @packages_for_template;
     for my $package ( @packages ) {
         
-        # show just a short excerpt of the text if it is too long
-        my ($text) = $package->opr_package_names->package_name;
-        $text      = substr( $text, 0, 37 ) . '...' if $params->{short} and 40 < length $text;
-        
-        # show just a short excerpt of the description if it is too long
-        my $desc = $package->description;
-        $desc    = substr( $desc, 0, 57 ) . '...' if $params->{short} and 60 < length $desc;
-        
-        my ($author) = $package->opr_user;
-        
         # create the infos for the template
-        push @packages_for_template, {
-            NAME        => $text,
-            VERSION     => $package->version,
-            DESCRIPTION => $desc,
-            AUTHOR      => ($author ? $author->user_name : '' ),
-            __SCRIPT__  => $self->base_url,
-            MYROLE      => '',  # TODO
-            DATE        => time_to_date( $self, $package->upload_time ),
-        };
+        push @packages_for_template, _package_to_hash( $self, $package, $params );
     }
     
     return ( \@packages_for_template, $pages );
+}
+
+sub version_list {
+    my ($self,$name,$params) = @_;
+    
+    return unless $name;
+    
+    my %search_clauses = (
+        'opr_package_names.package_name' => $name,
+    );
+        
+    if ( exists $params->{uploader} ) {
+        $search_clauses{uploaded_by} = $params->{uploader};
+    }
+    
+    if ( !$params->{all} ) {
+        $search_clauses{is_in_index} = 1;
+    }
+    
+    my $resultset = $self->table( 'opr_package' )->search(
+        {
+            %search_clauses,
+        },
+        {
+            order_by  => 'package_id',
+            join      => 'opr_package_names',
+            '+select' => [ 'opr_package_names.package_name' ],
+        },
+    );
+    
+    my @packages = $resultset->all;
+        
+    my @packages_for_template;
+    for my $package ( @packages ) {
+        
+        my $info = _package_to_hash( $self, $package, $params );
+        
+        if ( $package->deletion_flag ) {
+            $info->{DELETION_PRE}  = 'un';
+            $info->{DELETION_DATE} = time_to_date( $self, $package->deletion_flag );
+        }
+        
+        # create the infos for the template
+        push @packages_for_template, $info;
+    }
+    
+    return \@packages_for_template;
 }
 
 sub user_is_maintainer {
@@ -154,6 +208,33 @@ sub user_is_maintainer {
     }
     
     return;
+}
+
+sub _package_to_hash {
+    my ($self,$package,$params) = @_;
+        
+    # show just a short excerpt of the text if it is too long
+    my ($text) = $package->opr_package_names->package_name;
+    $text      = substr( $text, 0, 37 ) . '...' if $params->{short} and 40 < length $text;
+        
+    # show just a short excerpt of the description if it is too long
+    my $desc = $package->description;
+    $desc    = substr( $desc, 0, 57 ) . '...' if $params->{short} and 60 < length $desc;
+        
+    my ($author) = $package->opr_user;
+        
+    # create the infos for the template
+    my $info = {
+        NAME        => $text,
+        VERSION     => $package->version,
+        DESCRIPTION => $desc,
+        AUTHOR      => ($author ? $author->user_name : '' ),
+        __SCRIPT__  => $self->base_url,
+        DATE        => time_to_date( $self, $package->upload_time ),
+        PACKAGE_ID  => $package->package_id,
+    };
+    
+    return $info;
 }
 
 1;
