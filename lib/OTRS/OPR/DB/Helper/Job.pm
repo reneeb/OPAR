6bcc1c951fdb86bf5986f3cf806e83e5e66ffa3c
Renee Baecker <github@renee-baecker.de>
2011-02-16 03:00:49 -0800

    we use hashreferences for paramaters

--- lib/OTRS/OPR/DB/Helper/Job.pm
+++ lib/OTRS/OPR/DB/Helper/Job.pm
@@ -10,24 +10,24 @@ our @EXPORT_OK = qw(
 sub create_job {
     my ($self,$params) = @_;
     
-    return if !($params{id} and $params{type});
+    return if !($params->{id} and $params->{type});
     
     my ($type) = $self->table( 'opr_job_type' )->search({
-        type_label => $params{type},
+        type_label => $params->{type},
     })->all;
     
     return if !$type;
     
     my ($existing_job) = $self->table( 'opr_job_queue' )->search({
         type_id    => $type->type_id,
-        package_id => $params{id},
+        package_id => $params->{id},
     });
     
     return if $existing_job;
     
     my $job = $self->table( 'opr_job_queue' )->create({
         type_id    => $type->type_id,
-        package_id => $params{id},
+        package_id => $params->{id},
         created    => time,
     });
     
@@ -37,17 +37,17 @@ sub create_job {
 sub find_job {
     my ($self,$params) = @_;
     
-    return if !($params{id} and $params{type});
+    return if !($params->{id} and $params->{type});
     
     my ($type) = $self->table( 'opr_job_type' )->search({
-        type_label => $params{type},
+        type_label => $params->{type},
     })->all;
     
     return if !$type;
     
     my ($existing_job) = $self->table( 'opr_job_queue' )->search({
         type_id    => $type->type_id,
-        package_id => $params{id},
+        package_id => $params->{id},
     });
     
     return $existing_job;
