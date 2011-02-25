e8a8e26d3ff8df1efb599723fb1ab0ac67e11d52
Renee Baecker <github@renee-baecker.de>
2011-02-25 10:44:57 -0800

    add more logging stuff

--- lib/OTRS/OPR/DB/Helper/Passwd.pm
+++ lib/OTRS/OPR/DB/Helper/Passwd.pm
@@ -27,15 +27,21 @@ sub temp_passwd {
 sub check_temp_passwd {
     my ($self,$params) = @_;
     
+    $self->logger->trace( 'check_temp_passwd with token: ' . $params->{token} );
+    
     return if !$params->{token};
     
     my ($type) = $self->table( 'opr_temp_passwd' )->search({
         token => $params->{token},
     })->all;
     
+    $self->logger->trace( "Found $type" );
+    
     return if !$type;
     
     my $expire = $self->config->get( 'formid.passwd_expire' ) || 1200;
+    
+    $self->logger->trace( sprintf "User: %s Expire: %s Time: %s", $type->user_id, ($type->created + $expire), time );
     
     return if time > $type->created + $expire;
     return $type->user_id;
