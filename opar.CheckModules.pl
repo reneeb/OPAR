4c261e4b422c79c70e983d1e45f9ecb732015e63
Renee Baecker <github@renee-baecker.de>
2011-02-27 01:40:31 -0800

    make CheckModules script more convenient.

--- opar.CheckModules.pl
+++ opar.CheckModules.pl
@@ -3,12 +3,24 @@
 use strict;
 use warnings;
 
-my $ini_file = '/var/www/perl-services/opar/dist.ini';
+use File::Basename;
+
+no warnings 'redefine';
+no warnings 'prototype';
+
+my $dir      = dirname __FILE__;
+my $ini_file = $dir . '/dist.ini';
 open my $fh, '<', $ini_file or die $!;
 while ( my $line = <$fh> ) {
     chomp $line;
-    (my $file = $line) =~ s{::}{/}g;
-    $file .= '.pm';
-    eval{ require $file } or print "No $line: $@";
+
+    my ($module,$version) = split /\s*=\s*/, $line;
+
+    $version = 0 if !defined $version;
+
+    my $error;
+    eval "use $module $version; 1;" or $error = $@;
+
+    print sprintf "%-40s %s %s\n", $module, '.' x 15, ( $error || 'ok' );
 }
 close $fh;
