928100beba454968c327a2add8e149330baf2493
reneeb <github@renee-baecker.de>
2013-06-13 02:55:54 +0200

    fix bugs in get_frameworks.pl and make it more flexible (use env var to determine path to config file)

--- get_frameworks.pl
+++ get_frameworks.pl
@@ -16,9 +16,9 @@ use lib "$dir/lib";
 use OTRS::OPR::DB::Schema;
 use OTRS::OPR::Web::App::Config;
 
-my $configfile = Path::Class::File->new( $libdir, 'conf', 'base.yml' );
+my $configfile = $ENV{OPAR_CONFIG} || Path::Class::File->new( $dir, 'conf', 'base.yml' )->stringify;
 my $config     = OTRS::OPR::Web::App::Config->new(
-    $configfile->stringify,
+    $configfile,
 );
 
 my $db           = $config->get( 'db.name' );
@@ -29,7 +29,7 @@ my $schema       = OTRS::OPR::DB::Schema->connect(
     "DBI:$type:$db:$host",
     $config->get( 'db.user' ),
     $config->get( 'db.pass' ),
-    $schema,
+    $schema_class,
 );
 
 my @packages = $schema->resultset( 'opr_package' )->search();
@@ -37,6 +37,9 @@ my %frameworks;
 
 for my $package ( @packages ) {
     my $framework    = $package->framework;
+
+    next if !$framework;
+
     my @version_list = split /\s*,\s*/, $framework;
     my @shortened    = map{ my ($new) = $_ =~ m/(\d+.\d+)/; $new }@version_list;
 
