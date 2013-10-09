5003d7064d800893f7f966d714652e762c405548
reneeb <github@renee-baecker.de>
2013-10-09 23:05:31 +0200

    improve start/stop script

--- scripts/start_stop_starman.pl
+++ scripts/start_stop_starman.pl
@@ -23,7 +23,7 @@ my @ps        = `ps auwx`;
 my @processes = grep{ m[starman .*? --listen .*? opar\.psgi]xms }@ps;
 
 if ( @processes ) {
-    my ($cur_ip,$cur_port) =~ m{ --listen  .*? ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)?:([0-9]+) }xms;
+    my ($cur_ip,$cur_port) = $processes[0] =~ m{ --listen  .*? ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)?:([0-9]+) }xms;
 
     $ip   ||= $cur_ip || '';
     $port ||= $cur_port;
