0d52176fd09164e6637662490dd5f13f25ea4318
reneeb <github@renee-baecker.de>
2013-10-09 23:00:26 +0200

    improve start/stop script

--- scripts/start_stop_starman.pl
+++ scripts/start_stop_starman.pl
@@ -5,14 +5,26 @@ use warnings;
 
 use File::Basename;
 use File::Spec;
+use Getopt::Long;
 
-my $workers      = 10;
-my $max_requests = 200;
+GetOptions(
+    'workers:s'  => \my $workers,
+    'requests:s' => \my $max_requests,
+    'ip:s'       => \my $ip,
+    'port:s'     => \my $port,
+);
+
+$workers      //= 10;
+$max_requests //= 200;
+$ip           //= '';
+$port         //= 3050;
 
 my @ps        = `ps auwx`;
-my @processes = grep{ m/starman .*? --listen .*? :3050/xms }@ps;
+my @processes = grep{ m[starman .*? --listen .*? opar\.psgi]xms }@ps;
 
 if ( @processes ) {
+    ($ip,$port) =~ m{ --listen  .*? ([0-9]+\.[0-9]+\.[0-9]+\.[0-9])?:([0-9]+) }xms;
+
     my @pids = map{ m/ \A .*? (\d+) /xms; $1 }@processes;
 
     if ( @pids ) {
@@ -24,7 +36,9 @@ my $dir = File::Spec->rel2abs( dirname __FILE__ );
 
 chdir $dir;
 
-my $app = File::Spec->rel2abs( $dir, '..', 'opar', 'opar.psgi' );
+my $app = File::Spec->rel2abs(
+    File::Spec->catfile( $dir, '..', 'opar', 'opar.psgi' )
+);
 
 my $config = $ENV{OPAR_CONFIG};
 my $mode   = $ENV{MOJO_MODE};
@@ -39,9 +53,16 @@ if ($mode) {
 }
 
 # reminder for how to start the application
-print "\nRunning app with the following settings:\n";
-print "\tOPAR_CONFIG=$config\t# path to config\n";
-print "\tMOJO_MODE=$mode\t\t# production | development\n";
+print <<"TELL";
+
+Running app with the following settings:
+\tAPP=$app
+\tOPAR_CONFIG=$config\t# path to config
+\tMOJO_MODE=$mode\t\t# production | development
+\tLISTEN="$ip:$port"
+\tWORKERS=$workers
+\tMAX REQUESTS=$max_requests
+TELL
 
 my $exports = join '', map{ $_ . ' && ' }@exports;
 
